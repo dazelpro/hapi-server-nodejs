53ae2d7d6c650d68f7c13c803b4c99c59ffeae0d
dazelpro <dazelpro@gmail.com>
2021-04-16 10:57:12 +0700

    style guide eslint

--- src/handler.js
+++ src/handler.js
@@ -2,24 +2,35 @@ const { nanoid } = require('nanoid');
 const notes = require('./notes');
 
 const addNoteHandler = (req, res) => {
-    const { title, tags, body } = req.payload;
-    const id = nanoid(16);
-    const createdAt = new Date().toISOString();
-    const updatedAt = createdAt;
+  const { title, tags, body } = req.payload;
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
 
-    const newNote = {
-        title, tags, body, id, createdAt, updatedAt,
-    };
-    notes.push(newNote);
+  const newNote = {
+    title, tags, body, id, createdAt, updatedAt,
+  };
+  notes.push(newNote);
 
-    const isSuccess = notes.filter((note) => note.id === id).length > 0;
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
 
+  if (isSuccess) {
     const response = res.response({
-        status: 'fail',
-        message: 'Catatan gagal ditambahkan',
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
     });
-    response.code(500);
+    response.code(201);
     return response;
+  }
+  const response = res.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
 };
 
 module.exports = { addNoteHandler };
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 const { addNoteHandler } = require('./handler');
 
 const routes = [
-    {
-        method: 'POST',
-        path: '/notes',
-        handler: addNoteHandler,
-    },
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
 ];
 
 module.exports = routes;
