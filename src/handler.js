a5bde717b14db260fb133f90c0005c097d82300f
dazelpro <dazelpro@gmail.com>
2021-04-16 14:02:29 +0700

    delete notes

--- src/handler.js
+++ src/handler.js
@@ -92,6 +92,30 @@ const editNoteByIdHandler = (req, res) => {
   return response;
 };
 
+const deleteNoteByIdHandler = (req, res) => {
+  const { id } = req.params;
+  const index = notes.findIndex((note) => note.id === id);
+  if (index !== -1) {
+    notes.splice(index, 1);
+    const response = res.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+  const response = res.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
 module.exports = {
-  addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler,
+  addNoteHandler,
+  getAllNotesHandler,
+  getNoteByIdHandler,
+  editNoteByIdHandler,
+  deleteNoteByIdHandler,
 };
