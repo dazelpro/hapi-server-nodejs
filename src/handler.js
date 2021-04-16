b96d4b8d08862ed6c8c80842f786a9229cd7b2d6
dazelpro <dazelpro@gmail.com>
2021-04-16 13:51:38 +0700

    view detail note

--- src/handler.js
+++ src/handler.js
@@ -2,11 +2,13 @@ const { nanoid } = require('nanoid');
 const notes = require('./notes');
 
 const addNoteHandler = (req, res) => {
+  if (req.payload.title === '') {
+    req.payload.title = 'Untitled';
+  }
   const { title, tags, body } = req.payload;
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
   const updatedAt = createdAt;
-
   const newNote = {
     title, tags, body, id, createdAt, updatedAt,
   };
@@ -33,4 +35,33 @@ const addNoteHandler = (req, res) => {
   return response;
 };
 
-module.exports = { addNoteHandler };
+const getAllNotesHandler = () => ({
+  status: 'success',
+  data: {
+    notes,
+  },
+});
+
+const getNoteByIdHandler = (req, res) => {
+  const { id } = req.params;
+
+  const note = notes.filter((n) => n.id === id)[0];
+
+  if (note !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        note,
+      },
+    };
+  }
+
+  const response = res.response({
+    status: 'fail',
+    message: 'Catatan tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
